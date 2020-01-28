41ea27ea25f5c2f068ab5241d65e862093324ef9
sanjeevreddy <sanjeevreddy644@gmail.com>
2020-01-28 16:42:07 +0530

    change aws account

--- resources/aws.config.js
+++ resources/aws.config.js
@@ -1,8 +1,8 @@
 
 // AWS 
 let AWS = {       
-           accessKeyId : 'AKIAUE27L2UN2JBAFAPZ',
-           secretAccessKey: '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',
+           accessKeyId : 'AKIAVQIAZSPKA5HRGLI6',
+           secretAccessKey: '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',
            region: 'us-east-1'
           }
 
