6ee6f11f0e78142c361105c81786532459d1ab7d
sanjeevreddy <sanjeevreddy644@gmail.com>
2020-01-28 15:27:12 +0530

    changes

--- app.js
+++ app.js
@@ -31,9 +31,9 @@ app.use('/api/users', appuserAuthentication);
 
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function(err, req, res, next) {
