88a8dcdd8ea5f9c556d3a3c0d30c75d8680ea4f0
sanjeevreddy <sanjeevreddy644@gmail.com>
2020-01-28 12:19:13 +0530

    changes db config

--- resources/db.config.js
+++ resources/db.config.js
@@ -1,8 +1,8 @@
 
 
-dbConnectionURL = 'mongodb://localhost:27017/flunkeys'
+// dbConnectionURL = 'mongodb://localhost:27017/flunkeys'
 
-// dbConnectionURL = 'mongodb://macsof:macsof@ec2-54-157-52-31.compute-1.amazonaws.com:27017/nextlevelwash'
+ dbConnectionURL = 'mongodb://macsof:macsof@ec2-54-157-52-31.compute-1.amazonaws.com:27017/flunkey'
 
 module.exports = dbConnectionURL;
 
