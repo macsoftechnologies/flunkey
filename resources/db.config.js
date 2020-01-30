5ec179b270c6e73a69fe51043ed3ea1e092f73d5
sanjeevreddy <sanjeevreddy644@gmail.com>
2020-01-30 19:05:34 +0530

    changes dbConnection

--- resources/db.config.js
+++ resources/db.config.js
@@ -2,9 +2,9 @@
 
 // dbConnectionURL = 'mongodb://localhost:27017/flunkeys'
 
- dbConnectionURL = 'mongodb+srv://macsof:macsof@nextlevelcarwash-yjs3i.mongodb.net/flunkey'
+//  dbConnectionURL = 'mongodb+srv://macsof:macsof@nextlevelcarwash-yjs3i.mongodb.net/flunkey'
 
-// dbConnectionURL = 'mongodb://macsof:macsof@ec2-3-234-212-142.compute-1.amazonaws.com:27017/flunkey'
+dbConnectionURL = 'mongodb://macsof:macsof@ec2-18-208-187-60.compute-1.amazonaws.com:27017/flunkey'
 module.exports = dbConnectionURL;
 
 
